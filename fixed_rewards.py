d3d1658b4afc7540b5cfbedf4f87382644304c47
Monu-Chaudhary <chaudharymonu45@gmail.com>
2022-11-04 11:41:11 -0400

    updated equations based on feedback

--- fixed_rewards.py
+++ fixed_rewards.py
@@ -8,7 +8,9 @@ import csv
 from random import sample, randrange, random
 
 base_price = 0.001 #ETH
-k = 3
+varPrice = 0.1 #ETH
+
+k = 1
 
 #percentage of rewards
 athlete_reward_percent = 0.1
@@ -32,49 +34,42 @@ for i in range(athletes):
 def func_purchase_reward():
     rewards = []
     for i in range(athletes):
+        rewards.append(0)
+    for j in range(purchases):
         #price is fixed
-        reward = base_price * athlete_reward_percent * (popularity[i] * purchases) # number of purchases is proportional to popularity
-        rewards.append(reward)  
-    return rewards
-
-def func_purchase_reward_vary_price():
-    '''create a list to store the reward each athlete receives'''
-    rewards = []
-    for i in range(athletes):
-        price = base_price + base_price * pow(popularity[i],k)  #price is proportional to popularity
-        reward = price * athlete_reward_percent * (popularity[i] * purchases)   #number of purchase is proportional to popularity
-        rewards.append(reward)
+        idx = randrange(0,athletes)
+        rewards[idx] += base_price * athlete_reward_percent # number of purchases is random
     return rewards
 
 def func_exchange_reward_betn_2athletes():
     ex_rewards = []
     for i in range(athletes):
         ex_rewards.append(0)
-    for i in range(athletes):
-        for k in range(int(exchanges * popularity[i])):
+    for m in range(exchanges):
+        i = randrange(0,athletes)
+        j = randrange(0,athletes)
+        while(j == i):
             j = randrange(0,athletes)
-            while(j == i):
-                j = randrange(0,athletes)
-            diffPopularity = abs(popularity[i] - popularity[j])   #the more difference in popularity, the more price to pay for higher popular celeb
-            exchange_price = rand_float_range(0.001 * pow(diffPopularity,k), 0.01 * pow(diffPopularity,k)) # more difference implies exchange price range is larger and shifts to right
-            totalPopularity = popularity[i] + popularity[j]   #to find the fraction of exchange price that should go to each celeb
-            ex_rewards[i] += exchange_price * popularity[i]/totalPopularity * athlete_reward_percent
-            ex_rewards[j] += exchange_price * popularity[j]/totalPopularity * athlete_reward_percent   
+        sale_price1 = base_price + rand_float_range(0, varPrice * pow(popularity[i],k))
+        sale_price2 = base_price + rand_float_range(0, varPrice * pow(popularity[j],k))
+        exchange_price = abs(sale_price1 - sale_price2)   #the more difference in popularity, the more price to pay for higher popular celeb
+        totalPopularity = popularity[i] + popularity[j]   #to find the fraction of exchange price that should go to each celeb
+        ex_rewards[i] += exchange_price * popularity[i]/totalPopularity * athlete_reward_percent
+        ex_rewards[j] += exchange_price * popularity[j]/totalPopularity * athlete_reward_percent   
     return ex_rewards
 
-def func_auction():
+def func_exchange_reward_1athlete():
     ex_rewards = []
     for i in range(athletes):
         ex_rewards.append(0) 
-        #more popularity, more exchanges
-        for j in range(int(exchanges * popularity[i])):
-            exchange_price = 0.001 + rand_float_range(0.001 * pow(popularity[i],k), 0.1 * pow(popularity[i],k))
-            ex_rewards[i] += exchange_price * athlete_reward_percent
-
+    for j in range(exchanges):
+        i = randrange(0,athletes)
+        exchange_price = base_price + rand_float_range(0, varPrice * pow(popularity[i],k))
+        ex_rewards[i] += exchange_price * athlete_reward_percent
     return ex_rewards
 
 
-def func_exchange_reward_1athlete():
+def func_auction():
     au_rewards = []
     for i in range(athletes):
         au_rewards.append(0)
@@ -82,42 +77,36 @@ def func_exchange_reward_1athlete():
     #hence, number of auctions for a given athlete may not be proportional to the popularity of the athlete
     for i in range(auctions):
         idx = randrange(athletes)
-        winning_bid_price = 0.001 + rand_float_range(0.001 * pow(popularity[idx],k), 0.1 * pow(popularity[idx],k))
+        winning_bid_price = base_price + rand_float_range(0, varPrice * pow(popularity[idx],k))
         au_rewards[idx] += winning_bid_price * athlete_reward_percent
     return au_rewards
 
 with open('2.rewards_on_purchase.csv', 'a') as csvfile:
     spamwriter = csv.writer(csvfile)
     spamwriter.writerow(str(athletes) +' athletes, ' + str(purchases) + ' purchases')
-    # for i in range(100):
-    p_rewards = func_purchase_reward()
-    spamwriter.writerow(p_rewards)
-    spamwriter.writerow('')
-
-    spamwriter.writerow(str(athletes) +' athletes, ' + str(purchases) + ' purchases varying purchase price')
-    # for i in range(100):
-    vp_rewards = func_purchase_reward_vary_price()
-    spamwriter.writerow(vp_rewards)
+    for i in range(100):
+        p_rewards = func_purchase_reward()
+        spamwriter.writerow(p_rewards)
     spamwriter.writerow('')
 
 with open('2.rewards_on_exchange.csv', 'a') as csvfile:
     spamwriter = csv.writer(csvfile)
     spamwriter.writerow(str(athletes) +' athletes, ' + str(exchanges) + ' exchanges both athletes')
-    # for i in range(100):
-    ex_rewards2 = func_exchange_reward_betn_2athletes()
-    spamwriter.writerow(ex_rewards2)    
+    for i in range(100):
+        ex_rewards2 = func_exchange_reward_betn_2athletes()
+        spamwriter.writerow(ex_rewards2)    
     spamwriter.writerow('')
 
     spamwriter.writerow(str(athletes) +' athletes, ' + str(exchanges) + ' exchanges one athlete')
-    # for i in range(100):
-    ex_rewards1 = func_exchange_reward_1athlete()
-    spamwriter.writerow(ex_rewards1)    
+    for i in range(100):
+        ex_rewards1 = func_exchange_reward_1athlete()
+        spamwriter.writerow(ex_rewards1)    
     spamwriter.writerow('')
 
 with open('2.rewards_on_auction.csv', 'a') as csvfile:
     spamwriter = csv.writer(csvfile)
     spamwriter.writerow(str(athletes) +' athletes, ' + str(auctions))
-    # for i in range(100):
-    au_rewards = func_auction()
-    spamwriter.writerow(au_rewards)   
+    for i in range(100):
+        au_rewards = func_auction()
+        spamwriter.writerow(au_rewards)   
     spamwriter.writerow('')
--- plots/boxplot.py
+++ plots/boxplot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #add csv file to dataframe
-df = pd.read_csv('rewards.csv')
+df = pd.read_csv('2.3.rewards.csv')
 
 # df = df.div(1000)
 
@@ -11,7 +11,8 @@ boxplot = df.boxplot(figsize = (10,4), grid = False, showfliers = False)
 plt.xlabel("Average rewards in ETH for each celebrity")
 plt.ylabel("Number of celebrities and transactions")
 plt.xticks(rotation=30, ha='right')
+plt.yscale("log")
 plt.tight_layout()
 
 plt.show()
-plt.savefig("rewards.eps")
+plt.savefig("2.rewards_k=1.eps")
